b3b87b63e29750551c57280d78ac51ad4654a10e
sharvarihupare-369 <sharvarihupare15@gmail.com>
2025-02-11 23:41:07 +0530

    cors added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,8 +10,8 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to Base Rouet!");
